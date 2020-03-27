ccdf7765e5a64e6312f654e985a260d549f0cf0f
jinminsin <45217459+jinminsin@users.noreply.github.com>
2020-03-27 18:06:08 +0900

    어플리케이션 버전 18 이하 호환성 추가
    현황 : 어플리케이션 호환성 추가, 변수 통합
    필요 : 리스트뷰로 인한 디자인 적용성, 변수명 조율 필요

--- MainActivity.java
+++ MainActivity.java
@@ -1,37 +1,77 @@
 package com.koia.smartphonealldelete;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import android.os.Build;
 import android.os.Bundle;
-import android.os.Environment;
 import android.os.StatFs;
-import android.util.Log;
 import android.widget.TextView;
-
 import java.io.File;
 import java.text.DecimalFormat;
 
-
 public class MainActivity extends AppCompatActivity {
-    private TextView textSpace;
-    private String dir = Environment.getDataDirectory().getPath();
-
+    private TextView textSpace,textSpace2;
+    private TextView internal,external;
+    private File[] externalDrive;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        textSpace = findViewById(R.id.capacity);
-        textSpace.setText("(" + getFileSize(checkInternalStorageAllMemory(dir)-checkInternalAvailableMemory(dir)) + "/" + getFileSize(checkInternalStorageAllMemory(dir)) + ")");
+        if(Build.VERSION.SDK_INT > 18) {
+            externalDrive = getExternalFilesDirs(null);
+            String dir = externalDrive[0].getParentFile().getParentFile().getParentFile().getParentFile().getPath();
+
+            internal = findViewById(R.id.internal);
+            internal.setText(dir);
+            textSpace = findViewById(R.id.capacity);
+            textSpace.setText("(" + getFileSize(checkStorageAllMemory(dir) - checkAvailableMemory(dir)) + "/" + getFileSize(checkStorageAllMemory(dir)) + ")");
+
+            if (externalDrive.length > 1) {
+                dir = externalDrive[1].getParentFile().getParentFile().getParentFile().getParentFile().getPath();
+                external = findViewById(R.id.external);
+                external.setText(dir);
+                textSpace2 = findViewById(R.id.capacity2);
+                textSpace2.setText("(" + getFileSize(checkStorageAllMemory(dir) - checkAvailableMemory(dir)) + "/" + getFileSize(checkStorageAllMemory(dir)) + ")");
+            } else {
+                external = findViewById(R.id.external);
+                external.setText("대가리 깨짐");
+                textSpace2 = findViewById(R.id.capacity2);
+                textSpace2.setText("내 머리 머머리");
+            }
+        }else
+        {
+            externalDrive = ContextCompat.getExternalFilesDirs(this,null);
+            String dir = externalDrive[0].getParentFile().getParentFile().getParentFile().getParentFile().getPath();
+
+            internal = findViewById(R.id.internal);
+            internal.setText(dir);
+            textSpace = findViewById(R.id.capacity);
+            textSpace.setText("(" + getFileSize(checkStorageAllMemory(dir) - checkAvailableMemory(dir)) + "/" + getFileSize(checkStorageAllMemory(dir)) + ")");
+
+            if (externalDrive.length > 1) {
+                dir = externalDrive[1].getParentFile().getParentFile().getParentFile().getParentFile().getPath();
+                external = findViewById(R.id.external);
+                external.setText(dir);
+                textSpace2 = findViewById(R.id.capacity2);
+                textSpace2.setText("(" + getFileSize(checkStorageAllMemory(dir) - checkAvailableMemory(dir)) + "/" + getFileSize(checkStorageAllMemory(dir)) + ")");
+            } else {
+                external = findViewById(R.id.external);
+                external.setText("대가리 깨짐");
+                textSpace2 = findViewById(R.id.capacity2);
+                textSpace2.setText("내 머리 머머리");
+            }
+        }
     }
 
-    private long checkInternalStorageAllMemory(String dir) {
+    private long checkStorageAllMemory(String dir) {
         StatFs stat= new StatFs(dir);
-        return stat.getBlockSizeLong() *stat.getBlockCountLong();
+        return Build.VERSION.SDK_INT > 18 ? stat.getTotalBytes() : stat.getBlockSize() * stat.getBlockCount();
     }
 
-    private long checkInternalAvailableMemory(String dir) {
+    private long checkAvailableMemory(String dir) {
         StatFs stat= new StatFs(dir);
-        return stat.getBlockSizeLong() *stat.getAvailableBlocksLong();
+        return Build.VERSION.SDK_INT > 18 ? stat.getAvailableBytes() : stat.getAvailableBlocks() * stat.getBlockCount();
     }
 
     public String getFileSize(long size) {
