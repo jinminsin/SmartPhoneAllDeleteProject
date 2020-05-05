1218f5be3fbd51b5d782bbad9c51712d53af8e2d
hyuntaedo <44853842+hyuntaedo@users.noreply.github.com>
2020-05-05 19:58:20 +0900

    버튼 기능추가

--- java/com/example/myapplication/Wiping.java
+++ java/com/example/myapplication/Wiping.java
@@ -1,25 +1,49 @@
 package com.example.myapplication;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
 public class Wiping extends Activity {
     private TextView drive;
     private ProgressBar bar;
+    Button btn2;
+    Button btn3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wiping);
-
         drive = findViewById(R.id.drive);
-        bar = findViewById(R.id.progress);
 
-        DriveListItem k = (DriveListItem)getIntent().getSerializableExtra("DRIVE");
-        drive.setText(k.getDriveName()+"\n( "+k.getFileSize(k.getDriveFreeSize())+" / "+k.getFileSize(k.getDriveFullSize())+" )");
+        bar = findViewById(R.id.progress);
+        DriveListItem k = (DriveListItem) getIntent().getSerializableExtra("DRIVE");
+        drive.setText(k.getDriveName() + "\n( " + k.getFileSize(k.getDriveFreeSize()) + " / " + k.getFileSize(k.getDriveFullSize()) + " )");
         bar.setMax(100);
-        bar.setProgress((int)(100 * ((double)k.getDriveFreeSize()/(double)k.getDriveFullSize())));
+        bar.setProgress((int) (100 * ((double) k.getDriveFreeSize() / (double) k.getDriveFullSize())));
+    }
+
+
+
+
+    public void onClickButton(View view) {
+        Button btn2 = (Button)findViewById(R.id.returnmain);
+        Button btn3 = (Button)findViewById(R.id.turnoff);
+        if(btn2.getVisibility()  == View.VISIBLE && btn3.getVisibility() == View.VISIBLE){
+            btn2.setVisibility(View.INVISIBLE);
+            btn3.setVisibility(View.INVISIBLE);
+        }
+        else {
+            btn2.setVisibility(View.VISIBLE);
+            btn3.setVisibility(View.VISIBLE);
+        }
     }
 }
+
+
+
